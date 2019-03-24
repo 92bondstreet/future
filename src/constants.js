2324b2894f3424dd5468bbdeaca3a78f9ea6d944
Yassine AZZOUT <yassine.azzout@gmail.com>
2019-03-24 14:10:21 +0100

    chore(constants): add ats websites

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,12 @@
 const ATS_SAAS = [
   'https://jobs.lever.co',
-  'https://boards.greenhouse.io'
+  'https://boards.greenhouse.io',
+  'https://workable.com',
+  'https://smartrecruiters.com',
+  'https://recruitee.com',
+  'https://breezy.hr',
+  'https://jobs.jobvite.com',
+  'https://welcomekit.co'
 ];
 
 module.exports.ATS_SAAS = ATS_SAAS;
